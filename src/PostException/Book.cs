e418949ee6f9d58d7b28cb61906d0c77439b3f06
Martin Ortega <lynxestudio@outlook.com>
2023-10-12 01:11:30 -0400

    Add source code to project.

---
+++ src/PostException/Book.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PostException
+{
+    public class Book
+    {
+        public string Title { set; get; }
+        public int PubYear { set; get; }
+        public int NumPages { set; get; }
+        public string Authors { set; get; }
+    }
+}
---
+++ src/PostException/DataBaseException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Npgsql;
+
+namespace PostException
+{
+public class DataBaseException : ApplicationException
+{
+public DataBaseException(string message) : base(message) { }
+public DataBaseException(string message, NpgsqlException inner):base(message,inner) { }
+public DataBaseException(string message, Exception inner):base(message,inner) { }
+}
+}
